0a2f2296bae9d04702bcb9ce7a3ee6206c4885cc
Navodini Wijethilake <navodiniwijethilake@Navodinis-MacBook-Pro.local>
2020-07-02 07:19:21 +0530

    python scripts

--- Scripts_Python/Bead2_20200211/FCN/train.py
+++ Scripts_Python/Bead2_20200211/FCN/train.py
@@ -31,54 +31,6 @@ max_gt = 1
 model_path = "ckpt/deep_tfm/"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
-class HDF5Dataset(Dataset):
-    def __init__(self,img_dir, isTrain=True):
-        self.isTrain = isTrain
-       
-        #self.data_dict = pd.read_csv(data_dir) 
-        if isTrain: 
-            fold_dir = "train.txt"   
-        else: 
-            fold_dir = "test.txt"
-
-        ids = open(fold_dir, 'r')
-
-        self.index_list = []
-        
-        for line in ids:
-            self.index_list.append(line[0:-1])
-        self.img_dir = img_dir
-        #self.file = h5py.File(img_dir, 'r')
-        #self.cases, self.slices, self.nx, self.ny = self.file['input'].shape
-        #print(self.file['gt'].shape)
-        #dataset = []
-        #for i in self.file.values():
-        #    dataset.append(i)
-        #print(list(self.file.keys()))
-        #print(type(self.file['gt']))
-        #self.nx_gt, self.ny_gt, self.slices_gt, self.cases_gt = self.file['gt'].shape
-
-    def __len__(self):
-        return len(self.index_list)
-
-    def __getitem__(self, index):
-        _img = np.dtype('>u2') 
-        _target = np.dtype('>u2') 
-        id_ = int(self.index_list[index])
-        with h5py.File(self.img_dir, 'r') as db:
-             #print(db['input'].shape)
-             _img = db['input'][id_] 
-             _target = db['gt'][id_] 
-        if np.max(_target) == 0:
-             with h5py.File(self.img_dir, 'r') as db:
-                 _img = db['input'][id_+1]
-                 _target = db['gt'][id_+1]
-        _img = torch.from_numpy(np.divide(_img,max_im)).float()
-        _target = torch.from_numpy(np.divide(_target,max_gt)).float()
-
-        
-        return _img, _target
-
 class Dataset(Dataset):
     def __init__(self, img_dir):
         idx = 0
@@ -94,21 +46,12 @@ class Dataset(Dataset):
     def __getitem__(self, index):
         _img = np.zeros((128,128,32))
         _target = np.zeros((1,128,128))
-        #f_path = '/n/holysicratch01/wadduwage_lab/uom_bme/dataset_static_2020/20200105_synthBeads/tr_data_1sls/'
         f_path = '/n/holyscratch01/wadduwage_lab/uom_bme/dataset_static_2020/11-Feb-2020/tr_data_5sls/'
-        #print(self.img_anno_pairs[index])
         j=0
         for i in range(0,32):
             _img[:, :, j] = io.imread(f_path + self.img_anno_pairs[index]+'_'+str(i+1)+'.png')
-          #_img[:, :, j+1] = Image.open(f_path + self.img_anno_pairs[index] + '_' + 'pattern'+str(i+1) + '.png')
             j = j+1
-            #print(np.max(_img))
-            #_img[:, :, 1] = Image.open(self.img_anno_pairs[index] + '_flair.png')
         _target[0,:,:] = io.imread(f_path + self.img_anno_pairs[index] + '_gt.png')
-        #_target = np.array(_target)
-        #print('target',np.max(_target))
-        #_img = self.transform(np.array(_img, dtype=np.uint8))
-        #_target = self.transform(np.array(_target, dtype=np.uint8))
         if np.min(np.array(_target)) == np.max(np.array(_target)):
             print('Yes')
             k=0
@@ -116,10 +59,7 @@ class Dataset(Dataset):
                  _img[:, :, k] = io.imread(f_path + self.img_anno_pairs[index+1]+'_'+str(i+1)+'.png')
                  k = k+1
             _target[0,:,:] = io.imread(f_path + self.img_anno_pairs[index+1] + '_gt.png')
-        #_img = self.transform(np.array(_img, dtype=np.uint16))
-        #_target = self.transform(np.array(_target, dtype=np.uint16))
         _img = np.transpose(_img,(2,0,1))
-        #print(np.max(_img))
         _img = torch.from_numpy(np.divide(_img,max_im)).float()
         _target = torch.from_numpy(np.divide(_target,max_gt)).float()
 
@@ -133,13 +73,10 @@ def train(args, model, device, train_loader, optimizer, epoch, criterion):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        #loss = F.mae_loss(output, target)
         loss = criterion(output,target)
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
-        #pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        #correct += pred.eq(target.view_as(pred)).sum().item()
     
     train_loss /= len(train_loader.dataset)
     
@@ -154,14 +91,9 @@ def test(args, model, device, test_loader, criterion):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            #test_loss += F.mae_loss(output, target).item()  # sum up batch loss
             test_loss += criterion(output,target).item()
-            #test_loss += LabelSmoothingCrossEntropy().to(device)(output, target).item()
-            #pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            #correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    #report = classification_report(target.cpu(), pred.cpu(), labels= np.unique(pred.cpu().numpy()), digits=4)
     return test_loss
 
 
@@ -194,24 +126,8 @@ def main():
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
- #   weights = [2.21, 3.29, 1]
-    #weights = [0.019, 0.029, 0.0087]
-
-    #class_weights = torch.FloatTensor(weights).to(device)
-
+ 
     kwargs = {'num_workers': 2, 'pin_memory': True} if use_cuda else {}
-    
-
-    #Dataset making
-    """
-    train_X, train_Y = transorm_gene(k_fold, "train")
-    train_dataset = GeneDataset(torch.from_numpy(train_X), torch.from_numpy(train_Y))
-    
-
-    test_X, test_Y = transorm_gene(k_fold, "test")
-    test_dataset = GeneDataset(torch.from_numpy(test_X), torch.from_numpy(test_Y))"""
-    #img_dir = '/n/holyscratch01/wadduwage_lab/uom_bme/dataset_static_2020/cells_tr_data_6sls_17-Apr-2020.h5'
-    #img_dir = '/n/holyscratch01/wadduwage_lab/temp20200620/20-Jun-2020/beads_tr_data_5sls_20-Jun-2020.h5'
     
     train_dir = 'train.txt'
     test_dir = 'test.txt'
@@ -225,16 +141,10 @@ def main():
 
     test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0, drop_last=True)
 
-
-
-    #model = ResNet18().to(device)
     model = UNet(n_classes=args.output_nc).cuda()
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-6)
     scheduler = StepLR(optimizer, step_size=20, gamma=args.gamma)
     criterion = torch.nn.SmoothL1Loss()
-    #Training
-    #print("Training and Evaluation on Fold:", k_fold)
-    #print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
     Best_ACC = 0
     Best_Epoch = 1
 
@@ -242,7 +152,6 @@ def main():
     for epoch in range(1, args.epochs + 1):
         tloss= train(args, model, device, train_loader, optimizer, epoch,criterion)
         vloss= test(args, model, device, test_loader,criterion)
-#        if epoch == 1 or epoch == 11 or epoch == 21 or epoch == 31 or epoch == 41 or epoch == 51 or epoch == 61 or epoch == 71 or epoch == 81 or epoch == 91 or epoch == 101 or epoch == 111 or epoch == 121 or epoch == 131 or epoch == 141  :
         print("epoch:%.1f" %epoch, "Train_loss:%.4f" % tloss, "Val_loss:%.4f" % vloss)
         scheduler.step()
         try:
@@ -250,17 +159,7 @@ def main():
         except OSError:
             pass
         torch.save(model.state_dict(), model_path +"/fcn_deep_" + str(epoch) + ".pth")
-#    print("FOLD:", k_fold)
-#    print('Best-Epoch: {}, TRAIN-Accuracy: {:.2f}, TEST-Accuracy: {:.2f}'.format(Best_Epoch, Train_ACC, Best_ACC))
-
 
 if __name__ == '__main__':
 
-#K-fold
-    #for i in range(4):
     main()
-    #Average_Accuracy = sum(All_Accuracy)/len(All_Accuracy)
-    #print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-    #print("Fold-Epochs:", All_Epoch)
-    #print("Fold-Accuracy:", All_Accuracy)
-    #print("Average Test Accuracy: {:.2f}".format(Average_Accuracy))
--- Scripts_Python/Bead2_20200211/maxintensity.py
+++ Scripts_Python/Bead2_20200211/maxintensity.py
@@ -15,18 +15,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-#from networks import define_G, define_D, GANLoss, print_network
-#from data import get_training_set, get_test_set
-#import torch.backends.cudnn as cudnn
+
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as standard_transforms
-#from model import UNet
-#from Discriminator import Discriminator
 
-#from model import UNet
-#from torchvision.models import resnet18
-
-#os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
 args = {
     'num_class': 1,
     'ignore_label': 255,
@@ -52,23 +44,7 @@ args = {
 
 class Dataset(Dataset):
     def __init__(self, img_dir, transform=None):
-        #self.img_anno_pairs = glob(img_dir)
-        transforms_ = [standard_transforms.ToTensor()
-                       #,standard_transforms.Normalize((26.704), (49.92))]
-                       #standard_transforms.Normalize((25.704, 23.208), (49.92, 41.98))
-                        ]
 
-        self.transform = standard_transforms.Compose(transforms_)
-
-        transforms_target  = [standard_transforms.ToTensor()
-                       #,standard_transforms.Normalize((23.74), (45.17))
-                           ]
-
-        self.transform_target = standard_transforms.Compose(transforms_target)
-        # self.transform_img = standard_transforms.Compose(
-        #     standard_transforms.ToTensor(), standard_transforms.Normalize((26.704, 23.208), (49.92, 41.98)))
-        # self.transform_target = standard_transforms.Compose(
-        #     standard_transforms.ToTensor(), standard_transforms.Normalize((23.74), (45.17)))
         idx = 0
         file_img = open(img_dir, 'r')
         self.img_anno_pairs = {}
@@ -83,19 +59,13 @@ class Dataset(Dataset):
         _img = np.zeros((128,128,32))
         _target = np.zeros((1,128,128))
         f_path = '/n/holyscratch01/wadduwage_lab/uom_bme/dataset_static_2020/20200211_synthBeads_2/tr_data_6sls/'
-        #print(self.img_anno_pairs[index])
+        
         j=0
         for i in range(0,32):            
             _img[:, :, j] = io.imread(f_path + self.img_anno_pairs[index]+'_'+str(i+1)+'.png') 
-          #_img[:, :, j+1] = Image.open(f_path + self.img_anno_pairs[index] + '_' + 'pattern'+str(i+1) + '.png')
             j = j+1
-            #print(np.max(_img))
-            #_img[:, :, 1] = Image.open(self.img_anno_pairs[index] + '_flair.png')
         _target[0,:,:] = io.imread(f_path + self.img_anno_pairs[index] + '_gt.png')
-        #_target = np.array(_target)
-        #print('target',np.max(_target))
-        #_img = self.transform(np.array(_img, dtype=np.uint8))
-        #_target = self.transform(np.array(_target, dtype=np.uint8))
+
         if np.min(np.array(_target)) == np.max(np.array(_target)):
             print('Yes')
             k=0
@@ -103,18 +73,8 @@ class Dataset(Dataset):
                  _img[:, :, k] = io.imread(f_path + self.img_anno_pairs[index+1]+'_'+str(i+1)+'.png')
                  k = k+1
             _target[0,:,:] = io.imread(f_path + self.img_anno_pairs[index+1] + '_gt.png')
-        #_img = self.transform(np.array(_img, dtype=np.uint16))
-        #_target = self.transform(np.array(_target, dtype=np.uint16))
         _img = np.transpose(_img,(2,0,1))
 
-        #_img = self.transform(np.array(_img)[None, :, :])
-        #_target = self.transform(np.array(_target)[None, :, :])
-        #print(np.unique(np.array(_target)))
-        #print('img_size',_img.shape)
-        #print(np.unique(np.array(_img)))
-        #_img = torch.from_numpy(np.array(_img)[None, :, :]).float()
-        #_target = torch.from_numpy(np.array(_target)[None, :, :]).float()
-        #cv2.imwrite('test.png',_imgn)
         
         return _img, _target
 
@@ -126,7 +86,7 @@ training_data_loader = DataLoader(dataset=dataset_, batch_size=args['batch_size'
 max_im = 0
 max_gt = 0
 for iteration, batch in enumerate(training_data_loader, 1):
-   #print(iteration) 
+
    # forward
    real_a_cpu, real_b_cpu = batch[0], batch[1]
    max_a = np.max(real_a_cpu.cpu().detach().numpy())
@@ -136,6 +96,6 @@ for iteration, batch in enumerate(training_data_loader, 1):
    max_b = np.max(real_b_cpu.cpu().detach().numpy())
    if max_b > max_gt:
        max_gt = max_b
-#print(iteration)
+
 print(max_im)
 print(max_gt)
