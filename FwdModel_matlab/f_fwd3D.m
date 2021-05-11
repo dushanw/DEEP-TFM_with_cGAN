1b878a246387063e73f970506213d17a0ca0a7b2
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-05-11 17:09:35 +0530

    Added a preprocessing step to f_genobj_neuronPatches. Added an intensity based screening routine to f_fwd3D.m to make sure empty object volumes are not simulated

--- FwdModel_matlab/f_fwd3D.m
+++ FwdModel_matlab/f_fwd3D.m
@@ -18,10 +18,17 @@ function [Yhat Xgt] = f_fwd3D(X0,E,PSFs,pram)
   %   Xgt         - [y,x,t=1 ,b]
     
   %% preprocess inputs (for size, resolution, and dimentionality, and gpu-use)
-  %Nz_X0     = size(X0,3);
-  exPSF     = single(imresize3(PSFs.exPSF,PSFs.pram.dx/pram.dx,'Antialiasing',true));
-  emPSF     = single(imresize3(PSFs.emPSF,PSFs.pram.dx/pram.dx,'Antialiasing',true));
-  sPSF      = single(imresize3(PSFs.sPSF ,PSFs.pram.dx/pram.dx,'Antialiasing',true));
+  % Nz_X0   = size(X0,3);
+%   exPSF   =single(imresize3(PSFs.exPSF,PSFs.pram.dx/pram.dx,'Antialiasing',true));% same as the below
+%   emPSF   = single(imresize3(PSFs.emPSF,PSFs.pram.dx/pram.dx,'Antialiasing',true));
+%   sPSF    = single(imresize3(PSFs.sPSF ,PSFs.pram.dx/pram.dx,'Antialiasing',true));  
+  rsf_x     = PSFs.pram.dx/pram.dx;
+  rsf_y     = rsf_x;
+  rsf_z     = PSFs.pram.dx/pram.dz;  
+  exPSF     = single(imresize3(PSFs.exPSF,'Scale',[rsf_y,rsf_x,rsf_z],'Method','linear','Antialiasing',true));
+  emPSF     = single(imresize3(PSFs.emPSF,'Scale',[rsf_y,rsf_x,rsf_z],'Method','linear','Antialiasing',true));
+  sPSF      = single(imresize3(PSFs.sPSF ,'Scale',[rsf_y,rsf_x,rsf_z],'Method','linear','Antialiasing',true));
+  
   E         = reshape(E,[size(E,1),size(E,2),1,size(E,3)]);
   E_gt      = single(ones(pram.Ny,pram.Nx,1,1));
   %X0       = reshape(X0,[size(X0,1),size(X0,2),size(X0,3),1,size(X0,4)]);
@@ -39,13 +46,26 @@ function [Yhat Xgt] = f_fwd3D(X0,E,PSFs,pram)
   Eex_3D    = f_conv3nd(exPSF,E,'same');
   
   vol_Nz    = size(exPSF,3);
-  % vol_inits = [1:5:size(X0,3)-vol_Nz];
-  vol_inits = round(linspace(1,size(X0,3)-vol_Nz-1,10));
+  vol_inits = [1:5:size(X0,3)-vol_Nz];
+  % vol_inits = round(linspace(1,size(X0,3)-vol_Nz-1,10));
   
   Nb        = length(vol_inits);
+  meanX0    = mean(X0(:));
+  b_t       = 1;
   for b = 1:Nb
-    % b
     X0_vol  = X0(:,:,vol_inits(b):vol_inits(b)+vol_Nz-1);
+    if mean(X0_vol(:))>meanX0
+      vol_inits_valid(b_t) = vol_inits(b);
+      b_t   = b_t+1;
+    end
+  end
+  vol_inits = vol_inits_valid;
+  Nb        = b_t - 1;
+  
+  for b = 1:Nb
+    b
+    X0_vol  = X0(:,:,vol_inits(b):vol_inits(b)+vol_Nz-1);
+    X0_vol  = imrotate(X0_vol,90*rem(b,4));
     X_ex    = Eex_3D .* X0_vol;
     
     for j=1:size(X_ex,4)                              % using the loop in the function is slow for some reason
@@ -61,7 +81,7 @@ function [Yhat Xgt] = f_fwd3D(X0,E,PSFs,pram)
   
     %% postprocess (cropping)
     y_range   = round(size(X_em,1)/2 - pram.Nx/2)+1:round(size(X_em,1)/2 + pram.Nx/2);
-    x_range   = round(size(X_em,2)/2 - pram.Nx/2)+1:round(size(X_em,2)/2 + pram.Nx/2);    
+    x_range   = round(size(X_em,2)/2 - pram.Nx/2)+1:round(size(X_em,2)/2 + pram.Nx/2);
 
 %   Eex_3D    = Eex_3D   (y_range,x_range,:,:,:);
 %   X0_vol    = X0_vol   (y_range,x_range,:,:,:);
--- FwdModel_matlab/f_genobj_neuronPatches.m
+++ FwdModel_matlab/f_genobj_neuronPatches.m
@@ -1,10 +1,17 @@
 
 function X0 = f_genobj_neuronPatches(V_ps,pram)
+
+  %% preprocess neron volume
+  V_ps            = medfilt3(V_ps);                 % removee salt and pepper noise
+  V_ps            = V_ps - 3;                       % lower threshold = 3
+  V_ps(V_ps<0)    = 0;
+  V_ps(V_ps>20)   = 20;                             % upper threshold = 20
+
+  %%
+  Ny0             = pram.Ny;
+  Nx0             = pram.Nx;
   
-  Ny0         = pram.Ny;
-  Nx0         = pram.Nx;
-  
-  np          = 100;                                % The maximum photon value used for normarlization  
+  np              = 20;                             % The maximum photon value used for normarlization
   [y_inds,x_inds] = find(max(V_ps,[],3) > 0.2*np);  % Find object regions
 
   inds_valid      = (y_inds<size(V_ps,1)-Ny0-1) .* (x_inds<size(V_ps,2)-Nx0-1);
